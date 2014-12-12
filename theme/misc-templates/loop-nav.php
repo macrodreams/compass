565cde131eacfa7fcdb8144aec29571a39105701
robneu <rob@wpsitecare.com>
2014-12-12 00:49:50 -0600

    Revert "Merge pull request #33 from ccgjosh/develop"
    This reverts commit b9dc3f93d5876133a48ca0474f6d204fa5ddf9d8, reversing
    changes made to 6bea4f4827b465cdd7467c691e5b28918937208e.

--- theme/misc-templates/loop-nav.php
+++ theme/misc-templates/loop-nav.php
@@ -16,7 +16,7 @@
 	<nav class="nav-single">
 		<?php previous_post_link( '<span class="nav-previous">' . __( '%link', 'compass' ) . '</span>', '&larr; Previous Post' ); ?>
 		<?php next_post_link(     '<span class="nav-next">' . __( '%link', 'compass' ) . '</span>', 'Next Post &rarr;' ); ?>
-	</nav><!-- .nav-single -->
+	</nav><!-- .nav-singl -->
 
 	<?php
 
@@ -24,24 +24,11 @@ endif;
 
 if ( is_home() || is_archive() || is_search() ) :
 
-	if ( current_theme_supports( 'loop-pagination' ) ) :
-		loop_pagination(
-			array(
-				'prev_text' => '<span class="screen-reader-text">' . __( 'Previous Page', 'compass' ) . '</span>',
-				'next_text' => '<span class="screen-reader-text">' . __( 'Next Page', 'compass' ) . '</span>',
-			)
-		);
-	else : 
-		
-	?>
-	
-	<nav class="nav-archive">
-		<?php previous_post_link( '<span class="nav-previous">' . __( '%link', 'compass' ) . '</span>', '&larr; Older Posts' ); ?>
-		<?php next_post_link(     '<span class="nav-next">' . __( '%link', 'compass' ) . '</span>', 'Newer Posts &rarr;' ); ?>
-	</nav><!-- .nav-archive -->
-	
-	<?php
-	
-	endif;
+	loop_pagination(
+		array(
+			'prev_text' => '<span class="screen-reader-text">' . __( 'Previous Page', 'compass' ) . '</span>',
+			'next_text' => '<span class="screen-reader-text">' . __( 'Next Page', 'compass' ) . '</span>',
+		)
+	);
 
 endif;
