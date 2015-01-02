3244bf1b964d4776738622ba26de499f64c6ffb9
robneu <rob@wpsitecare.com>
2015-01-02 16:57:10 -0600

    Fixed bourbon files location for 4.1
    Bourbon changed their directory structure in version 4.1, so moving
    forward we need to point to the correct location when copying.

--- grunt/config/bowercopy.js
+++ grunt/config/bowercopy.js
@@ -8,7 +8,7 @@ module.exports = {
 			destPrefix: '<%= paths.bower %>'
 		},
 		files: {
-			bourbon: 'bourbon/dist',
+			bourbon: 'bourbon/app/assets/stylesheets',
 			neat: 'neat/app/assets/stylesheets',
 			genericons: 'genericons/genericons',
 			'wp-normalize': 'wp-normalize.scss/_wp-normalize.scss'
