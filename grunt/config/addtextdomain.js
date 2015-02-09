4416fcb8e91e9e0952418ee5d8844330451b52db
robneu <rob@wpsitecare.com>
2015-02-09 06:38:25 -0600

    Simplified addtextdomain task
    Because this task now allows an ‘all’ flag, I think this makes for a
    better default setting. Now all textdomains will be updated on build
    except for Hybrid Core.
    
    Be sure to add exceptions for your own projects as-necessary.

--- grunt/config/addtextdomain.js
+++ grunt/config/addtextdomain.js
@@ -2,23 +2,13 @@
 module.exports = {
 	options: {
 		textdomain: '<%= pkg.theme.textdomain %>',
-		updateDomains: ['compass'] // Hard-coded by default
+		updateDomains: ['all']
 	},
 	php: {
 		files: {
 			src: [
 				'<%= files.php %>',
 				'!<%= paths.hybridCore %>**/*.php'
-			]
-		}
-	},
-	flagshiplibrary: {
-		options: {
-			updateDomains: ['flagship-library']
-		},
-		files: {
-			src: [
-				'<%= paths.theme %>includes/vendor/flagship-library/**/*.php'
 			]
 		}
 	}
