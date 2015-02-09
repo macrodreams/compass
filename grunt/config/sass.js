f9485ceb397c227f3affff44f8790a2bbc617278
robneu <rob@wpsitecare.com>
2015-02-09 03:56:50 -0600

    Removed banners from Sass build
    These are no longer supported by grunt-contrib-sass

--- grunt/config/sass.js
+++ grunt/config/sass.js
@@ -8,22 +8,6 @@ module.exports = {
 		lineNumbers: false
 	},
 	theme: {
-		options: {
-			banner:
-				'/*!\n' +
-				'Theme Name:  <%= pkg.theme.name %>\n' +
-				'Version:     <%= pkg.version %>\n' +
-				'Author:      <%= pkg.theme.author %>\n' +
-				'Author URI:  <%= pkg.theme.authoruri %>\n' +
-				'Theme URI:   <%= pkg.theme.uri %>\n' +
-				'Description: <%= pkg.theme.description %>\n' +
-				'Tags:        <%= pkg.theme.tags %>\n' +
-				'Text Domain: <%= pkg.theme.textdomain %>\n' +
-				'Domain Path: <%= pkg.theme.domainpath %>\n' +
-				'License:     <%= pkg.theme.license %>\n' +
-				'License URI: <%= pkg.theme.licenseuri %>\n' +
-				'*/\n'
-		},
 		files: [
 			{
 				expand: true,
@@ -35,14 +19,6 @@ module.exports = {
 		]
 	},
 	editorstyle: {
-		options: {
-			lineNumbers: false,
-			banner:
-				'/*!\n' +
-				'<%= pkg.theme.name %> Editor Styles\n' +
-				'Version: <%= pkg.version %>\n' +
-				'*/\n'
-		},
 		files: [
 			{
 				expand: true,
